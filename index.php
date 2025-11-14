fde0451ce3329739fe97bf7b7eee9edad85bfd75
Remruata <erp@mzhssp.in>
2025-11-15 03:22:07 +0530

    update favicon and site title

--- index.php
+++ index.php
@@ -6,12 +6,12 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Lushai Media - Index</title>
+  <title>Home - Lushai Media</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/logo-mini.png" rel="icon">
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
